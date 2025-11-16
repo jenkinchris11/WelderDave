b9d0c014555f8a7b51094e7bc0b9f49936246f63
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-11-16 19:04:37 +0000

    Update GoogleReviews.vue

--- vue-netlify/src/components/GoogleReviews.vue
+++ vue-netlify/src/components/GoogleReviews.vue
@@ -20,7 +20,7 @@ const embedLink = `${googleReviewLink}?embed=1`;
 
     <div class="reviews__embed">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2359.074748593415!2d-2.3619213232434344!3d53.75255207240996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b99189bb89acb%3A0xd6b6471380a38543!2sWelderDave%20-%20Accrington%20Welding%20%26%20Fabrication!5e0!3m2!1sen!2suk!4v1763318812359!5m2!1sen!2suk" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      <iframe src="https://maps.app.goo.gl/fRWtYpLjmx9dssLo7"><iframe/>
+      <iframe src="https://maps.app.goo.gl/fRWtYpLjmx9dssLo7"></iframe>
     </div>
   </section>
 </template>

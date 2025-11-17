fdaf49c9d92f6e4c91e8e384c8f8da23b4204766
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-11-17 14:04:07 +0000

    Update ElfsightReviews.vue

--- vue-netlify/src/components/ElfsightReviews.vue
+++ vue-netlify/src/components/ElfsightReviews.vue
@@ -40,6 +40,7 @@ onMounted(() => {
   border-radius: 1.25rem;
   box-shadow: 0 35px 65px -45px rgba(15, 23, 42, 0.4);
 }
+.elfsight-reviews a { display:none; }
 
 .elfsight-reviews__eyebrow {
   margin-bottom: 1rem;

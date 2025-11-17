a87070640b51d00fe2148fdde3a0537cce0a2a88
jenkinchris11 <CJ204077@falmouth.ac.uk>
2025-11-17 17:11:10 +0000

    Update FeatureGrid.vue

--- vue-netlify/src/components/FeatureGrid.vue
+++ vue-netlify/src/components/FeatureGrid.vue
@@ -60,11 +60,11 @@ const features = [
 }
 
 .eyebrow {
-  font-size: 0.9rem;
   letter-spacing: 0.2em;
   text-transform: uppercase;
   font-weight: 700;
   color: #FF761A;
+  text-align: center;
 }
 
 h3 {
